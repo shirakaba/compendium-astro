5d2074f192e58d67279a0ef9d5bc0f8358a3fda3
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-04-19 12:08:13 +0900

    set up test

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,8 +1,12 @@
 import type { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
+// Any changes saved in a .tsx file will:
+// - SSR: cause a page reload.
+// - CSR: apply by HMR.
 export function Example({
   children,
   className,
 }: PropsWithChildren<{ className?: string; }>){
-  return (<h1 className={className}>{children}</h1>);
+  return (<h1 className={twMerge("text-4xl", className)}>{children}</h1>);
 }
