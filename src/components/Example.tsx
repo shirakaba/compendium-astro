9643cf2e8dc5be473fe1c7e0434a171a37a2faaa
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-04-19 12:03:45 +0900

    supply pages/index.astro
    following https://docs.astro.build/en/install-and-setup/#manual-setup

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,5 +1,8 @@
 import type { PropsWithChildren } from "react";
 
-export function Example({ children, className } : PropsWithChildren<{ className?: string; }>){
+export function Example({
+  children,
+  className,
+}: PropsWithChildren<{ className?: string; }>){
   return (<h1 className={className}>{children}</h1>);
 }
