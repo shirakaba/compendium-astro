d98c59a2413100267d690aafcbadf07f1ed08fb8
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-05-21 14:07:55 +0900

    use kebab-case

--- src/scratchpad.tsx
+++ src/scratchpad.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
 import './scratchpad.css';
-import { ScratchpadApp } from './scratchpadApp.tsx';
+import { ScratchpadApp } from './scratchpad-app.tsx';
 
 createRoot(document.getElementById('root')!).render(<ScratchpadApp />);
