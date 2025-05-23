262827ad18f6596d9023ba8359e8a0564e97989c
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-05-24 08:13:22 +0900

    fix Prettier integration

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -5,8 +5,15 @@
 const config = {
   attributeGroups: ['^(id|name)$', '^data-', '^class$', '$DEFAULT'],
   bracketSpacing: true,
-  plugins: ['prettier-plugin-organize-attributes', 'prettier-plugin-astro'],
+  plugins: [
+    'prettier-plugin-organize-attributes',
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-astro',
+  ],
+  pluginSearchDirs: false,
   singleQuote: true,
+  tailwindFunctions: ['classes'],
+  tailwindStylesheet: 'src/styles/global.css',
   trailingComma: 'es5',
 };
 
