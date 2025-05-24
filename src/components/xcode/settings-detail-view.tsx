0154d51105684f5fad3b00d8a1b28d390bcd6cbf
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-05-24 13:10:38 +0900

    enable Build Phases to open and close

--- src/components/xcode/settings-detail-view.tsx
+++ src/components/xcode/settings-detail-view.tsx
@@ -1,48 +1,101 @@
 import Add from '@material-design-icons/svg/round/add.svg?react';
 import ArrowForwardIos from '@material-design-icons/svg/round/arrow_forward_ios.svg?react';
+import { useState } from 'react';
 
 import { Icon } from './icon-button';
 
 export function SettingsDetailView() {
+  const [phases, setPhases] = useState<
+    Array<{ title: string; isOpen?: boolean }>
+  >([
+    { title: 'Target Dependencies (0 items)' },
+    { title: 'Run Build Tool Plug-ins (0 items)' },
+    { title: '[CP] Check Pods Manifest.lock' },
+    { title: '[Expo] Configure project' },
+    { title: 'Compile Sources (4 items)' },
+    { title: 'Link Binary With Libraries (1 item)' },
+    { title: 'Copy Bundle Resources (5 items)' },
+    { title: 'Bundle React Native code and images' },
+    { title: '[CP] Copy Pods Resources' },
+    { title: '[CP] Embed Pods Frameworks' },
+  ]);
+
   return (
     <div className="flex w-full flex-col">
       <div className="flex h-7.5 items-center border border-transparent border-b-appkit-divider-minor px-[5px]">
         <Icon SVG={Add} className="size-5" />
       </div>
       <div className="flex flex-col px-5">
-        <BuildPhase title="Target Dependencies (0 items)" />
-        <BuildPhase title="Run Build Tool Plug-ins (0 items)" />
-        <BuildPhase title="[CP] Check Pods Manifest.lock" />
-        <BuildPhase title="[Expo] Configure project" />
-        <BuildPhase title="Compile Sources (4 items)" />
-        <BuildPhase title="Link Binary With Libraries (1 item)" />
-        <BuildPhase title="Copy Bundle Resources (5 items)" />
-        <BuildPhase title="Bundle React Native code and images" />
-        <BuildPhase title="[CP] Copy Pods Resources" />
-        <BuildPhase title="[CP] Embed Pods Frameworks" />
+        {phases.map(({ title, isOpen }, i) => (
+          <BuildPhase
+            key={i}
+            isOpen={isOpen}
+            title={title}
+            onClickHeader={() =>
+              setPhases((phases) =>
+                phases.map((phase, j) => ({
+                  ...phase,
+                  isOpen: j === i ? !phase.isOpen : phase.isOpen,
+                }))
+              )
+            }
+          />
+        ))}
       </div>
     </div>
   );
 }
 
-function BuildPhase({
-  isOpen: _isOpen,
-  title,
-}: {
-  isOpen?: boolean;
-  title: string;
-}) {
+function BuildPhase({ isOpen, onClickHeader, title }: BuildPhaseProps) {
   return (
-    <div className="flex h-10 items-center gap-x-2 border border-transparent border-b-appkit-divider-minor">
+    <>
+      <BuildPhaseHeader isOpen={isOpen} title={title} onClick={onClickHeader} />
+      {isOpen && <BuildPhaseBody />}
+    </>
+  );
+}
+
+interface BuildPhaseProps
+  extends Omit<BuildPhaseHeaderProps, 'onClick'>,
+    BuildPhaseBodyProps {
+  onClickHeader?: BuildPhaseHeaderProps['onClick'];
+}
+
+function BuildPhaseHeader({ isOpen, title, onClick }: BuildPhaseHeaderProps) {
+  return (
+    <div
+      {...(isOpen ? { 'data-open': '' } : {})}
+      className="group flex h-10 items-center gap-x-2 border border-transparent border-b-appkit-divider-minor"
+      onClick={onClick}
+    >
       <Icon
         SVG={ArrowForwardIos}
-        className="size-2.5 text-xcode-accordion-arrow active:text-xcode-accordion-arrow-active"
+        className="size-2.5 text-xcode-accordion-arrow group-data-open:rotate-90 active:text-xcode-accordion-arrow-active"
       />
       {/*
         Problem: Now that we've removed Tailwind Preflight for the sake of
-        Astro, this has user agent styles to fight against
+        Astro, this has user agent styles to fight against. Maybe we could
+        fork Preflight and select on .not-content.
       */}
-      <h1 className="text-xs">{title}</h1>
+      <h1 className="cursor-default text-xs select-none">{title}</h1>
     </div>
   );
+}
+
+interface BuildPhaseHeaderProps extends Pick<BuildPhaseInfo, 'title'> {
+  isOpen?: boolean;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+function BuildPhaseBody() {
+  return <div>Hi</div>;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface BuildPhaseBodyProps {
+  // TODO
+}
+
+interface BuildPhaseInfo {
+  title: string;
 }
