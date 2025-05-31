7da5fa050a2fc5ec2b7d10c16f64843247955c27
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-05-31 17:25:52 +0900

    view transitions

--- src/components/xcode/window.tsx
+++ src/components/xcode/window.tsx
@@ -1,4 +1,12 @@
-import { useRef, useState } from 'react';
+/// <reference types="react/experimental" />
+
+import {
+  useRef,
+  useState,
+  unstable_ViewTransition as ViewTransition,
+  startTransition,
+} from 'react';
+import { createPortal } from 'react-dom';
 import { twMerge } from 'tailwind-merge';
 
 import { ProjectSelectionBar } from './project-selection-bar';
@@ -12,22 +20,47 @@ export function XcodeWindow({
   className,
   ...props
 }: { withoutPopover?: boolean } & Omit<XcodeWindowInnerProps, 'isPopover'>) {
+  const [fullSize, setFullSize] = useState(false);
+
+  // const transitionName = 'xcode-window';
+
   return (
     // I introduced this container <div> just to handle Astro's `.not-content`
     // failing to match on the popover (for reasons I can't explain).
     <div className={className}>
-      {/* This one reserves space in the DOM tree. */}
-      <XcodeWindowInner {...props} isPopover={false} />
-
-      {/* This one is the popover. */}
-      {!withoutPopover && <XcodeWindowInner {...props} isPopover={true} />}
+      {/* https://github.com/facebook/react/blob/ee76351917106c6146745432a52e9a54a41ee181/fixtures/view-transition/src/components/Page.js#L112 */}
+      {fullSize ? (
+        createPortal(
+          <ViewTransition
+          // default="enter-slide-right exit-slide-left"
+          // name={transitionName}
+          >
+            <XcodeWindowInner
+              {...props}
+              className="fixed inset-0 z-50"
+              onZoom={() => setFullSize((fullsize) => !fullsize)}
+            />
+          </ViewTransition>,
+          document.body
+        )
+      ) : (
+        <ViewTransition
+        // default="enter-slide-right exit-slide-left"
+        // name={transitionName}
+        >
+          <XcodeWindowInner
+            {...props}
+            onZoom={() => setFullSize((fullsize) => !fullsize)}
+          />
+        </ViewTransition>
+      )}
     </div>
   );
 }
 
 function XcodeWindowInner({
   className,
-  isPopover,
+  onZoom,
   ...props
 }: XcodeWindowInnerProps) {
   const ref = useRef<HTMLDivElement>(null);
@@ -38,82 +71,20 @@ function XcodeWindowInner({
     <div
       ref={ref}
       {...props}
-      popover={isPopover ? 'manual' : undefined}
       // TODO: would be nice to animate the full-screen backdrop
       // illuminating/dimming independently of the Xcode window
       className={twMerge(
-        // Appearance
         'pt-appkit-window-shadow-top pr-appkit-window-shadow-right pb-appkit-window-shadow-bottom pl-appkit-window-shadow-left',
-
-        // Animation
-        'transition-all transition-discrete duration-500',
-
-        // Popover open
-        'open:size-full starting:open:top-(--starting-top) starting:open:left-(--starting-left) starting:open:h-(--starting-height) starting:open:w-(--starting-width)',
-
-        // Popover closed
-        isPopover &&
-          'top-(--ending-top) left-(--ending-left) h-(--ending-height) w-(--ending-width)',
         className
       )}
     >
       <div className="relative flex h-40 min-h-[360px] resize flex-col overflow-hidden rounded-lg bg-appkit-title-bar text-sm text-black shadow-appkit-window dark:text-white">
         <TitleBar
           onZoom={() => {
-            const popover = isPopover
-              ? ref.current
-              : ref.current?.nextElementSibling;
-            if (!(popover instanceof HTMLDivElement)) {
-              return;
-            }
-
-            const inFlow = popover.previousElementSibling;
-            if (!(inFlow instanceof HTMLDivElement)) {
-              return;
-            }
-
-            const inFlowResizer =
-              inFlow.querySelector<HTMLDivElement>('.resize');
-            const popoverResizer =
-              popover.querySelector<HTMLDivElement>('.resize');
-
-            if (!inFlowResizer || !popoverResizer) {
-              return;
-            }
-
-            // Get the initial dimensions so that we can animate with
-            // `@starting-style`.
-            const { width, height, top, left } = inFlow.getBoundingClientRect();
-
-            // The popover handles the close, while the in-flow component
-            // handles the open.
-            if (isPopover) {
-              inFlowResizer.style.width = popoverResizer.style.width;
-              inFlowResizer.style.height = popoverResizer.style.height;
-
-              popover.style.setProperty('--ending-top', `${top}px`);
-              popover.style.setProperty('--ending-left', `${left}px`);
-              popover.style.setProperty('--ending-width', `${width}px`);
-              popover.style.setProperty('--ending-height', `${height}px`);
-              popover.hidePopover();
-            } else {
-              popoverResizer.style.width = inFlowResizer.style.width;
-              popoverResizer.style.height = inFlowResizer.style.height;
-
-              popover.style.setProperty('--starting-top', `${top}px`);
-              popover.style.setProperty('--starting-left', `${left}px`);
-              popover.style.setProperty('--starting-width', `${width}px`);
-              popover.style.setProperty('--starting-height', `${height}px`);
-              popover.style.setProperty('--ending-top', '0px');
-              popover.style.setProperty('--ending-left', '0px');
-              popover.style.setProperty('--ending-width', '100%');
-              popover.style.setProperty('--ending-height', '100%');
-              popover.showPopover();
-
-              // On Safari, the popover fails to lay out properly before 18.4.
-              // https://webkit.org/blog/16574/webkit-features-in-safari-18-4/
-              // https://github.com/WebKit/WebKit/commit/4b91c7fe1375dbfc4af6c5d8e3ebe23d6589f895
-            }
+            startTransition(async () => {
+              await new Promise<void>((resolve) => setTimeout(resolve, 500));
+              onZoom?.();
+            });
           }}
         />
         <div className="flex bg-appkit-content-view">
@@ -150,5 +121,5 @@ type XcodeWindowInnerProps = React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 > & {
-  isPopover: boolean;
+  onZoom?: () => void;
 };
